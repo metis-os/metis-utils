14d6bf4844349dd4089b0fc7e4e4b76101939eb3
iyamnabeen <imnabeen01@gmail.com>
2022-08-12 19:27:01 +0545

    add: build script + readme + license

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+## Copyright (C) 2021-2022 Nabeen Tiwaree <iym.nabeen@gmail.com>
+## Everyone is permitted to copy and distribute copies of this file under GNU-GPL3
+
+#dir definations
+PKGDIR=~/projects/metis/metis-repo/
+dir_check(){
+	if [[ ! -d "$PKGDIR" ]]; then
+		mkdir -p "$PKGDIR"
+	fi
+}
+
+build_pkg(){
+    cd metis-dmenu && makepkg -s && mv *.pkg.tar.zst "$PKGDIR" && rm -rf src pkg
+    cd ../metis-slstatus && makepkg -s && mv *.pkg.tar.zst "$PKGDIR" && rm -rf src pkg
+    cd ../metis-st && makepkg -s && mv *.pkg.tar.zst "$PKGDIR" && rm -rf src pkg 
+
+}
+dir_check
+build_pkg && echo -e "*.......PKGS BUILT SUCCESSFULLY.......*" || echo -e "*.......ERROR occured.......*"
+

43b84244775c153b460849977e5d023dbc97aa0d
Safal Piya <mrsafalpiya@gmail.com>
2022-12-01 10:15:23 +0545

    Added `center` patch.
    From `https://tools.suckless.org/dmenu/patches/center/`.

--- metis-dmenu/files/config.h
+++ metis-dmenu/files/config.h
@@ -1,4 +1,6 @@
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 500;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
 	"JetBrainsMono Nerd Font=Regular:size=18",
